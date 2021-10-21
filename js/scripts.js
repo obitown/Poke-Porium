e0d8f6e9b7f3755e37ba35f281313bbf98290d5a
obitown <92471740+obitown@users.noreply.github.com>
2021-10-21 11:58:02 -0700

    add Files to Poke-Porium

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//test
+alert('Hello World! Welcome to Poké-Porium!');
+
+//pokemon
+let pokemonName='Pikachu-';
+
+document.write(pokemonName);
+
+//Favorite Food
+let favoriteFood='Poffin';
+
+document.write(favoriteFood);
+
+
+

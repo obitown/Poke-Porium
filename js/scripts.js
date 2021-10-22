b99bcc4ba9479784b34b550131b279adec1d0192
obitown <92471740+obitown@users.noreply.github.com>
2021-10-22 12:15:49 -0700

    Add "Pokemon list" to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 //test
-alert('Hello World! Welcome to Poké-Porium!');
+alert('Hello World! Welcome to Poké-Porium!')
 
-//pokemon
-let pokemonName='Pikachu-';
-
-document.write(pokemonName);
-
-//Favorite Food
-let favoriteFood='Poffin';
-
-document.write(favoriteFood);
-
-
-
+let pokemonList = [
+    
+    {name: 'Bulbasaur', height: 0.7, types:['Monster','Grass']},
+    
+    {name: 'Ivysaur', height: 1, types:['Monster','Grass']},
+    
+    {name: 'Venusaur', height: 2, types: ['Monster','Grass']},
+    
+    {name: 'Charmander', height: 0.6, types: ['Monster','Dragon']},
+    
+    {name: 'Charmeleon', height: 1.1, types: ['Monster','Dragon']},
+    
+    {name: 'Charizard', height: 1.7, types: ['Monster','Dragon']},
+    
+    {name: 'Squirtle', height: 0.5, types: ['Monster','Water']},
+    
+    {name: 'Wartortle', height: 1, types: ['Monster','Water']},
+    
+    {name: 'Blastoise', height: 1.6, types: ['Monster','Water']},
+    
+]

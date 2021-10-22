c3d85877f3e24e30670d01513811d8a70d346c48
obitown <92471740+obitown@users.noreply.github.com>
2021-10-22 12:28:50 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,22 @@
 alert('Hello World! Welcome to Poké-Porium!')
 
 let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types:['Monster', 'Grass']},
     
-    {name: 'Bulbasaur', height: 0.7, types:['Monster','Grass']},
+    {name: 'Ivysaur', height: 1, types:['Monster', 'Grass']},
     
-    {name: 'Ivysaur', height: 1, types:['Monster','Grass']},
+    {name: 'Venusaur', height: 2, types: ['Monster', 'Grass']},
     
-    {name: 'Venusaur', height: 2, types: ['Monster','Grass']},
+    {name: 'Charmander', height: 0.6, types: ['Monster', 'Dragon']},
     
-    {name: 'Charmander', height: 0.6, types: ['Monster','Dragon']},
+    {name: 'Charmeleon', height: 1.1, types: ['Monster', 'Dragon']},
     
-    {name: 'Charmeleon', height: 1.1, types: ['Monster','Dragon']},
+    {name: 'Charizard', height: 1.7, types: ['Monster', 'Dragon']},
     
-    {name: 'Charizard', height: 1.7, types: ['Monster','Dragon']},
+    {name: 'Squirtle', height: 0.5, types: ['Monster', 'Water']},
     
-    {name: 'Squirtle', height: 0.5, types: ['Monster','Water']},
+    {name: 'Wartortle', height: 1, types: ['Monster', 'Water']},
     
-    {name: 'Wartortle', height: 1, types: ['Monster','Water']},
-    
-    {name: 'Blastoise', height: 1.6, types: ['Monster','Water']},
+    {name: 'Blastoise', height: 1.6, types: ['Monster', 'Water']},
     
 ]
